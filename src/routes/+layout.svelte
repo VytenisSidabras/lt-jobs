61254fd99c63f2984b6d8f98fca6ab5705d9af84
Vytenis Sidabras <vytenis@zyro.com>
2023-06-01 22:55:39 +0300

    add plausible data collection script

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,13 @@
   import "./styles.css";
 </script>
 
+<svelte:head>
+  <script
+    defer
+    data-domain="whatisoffered.lt"
+    src="https://plausible.io/js/script.js"
+  ></script>
+</svelte:head>
 <div class="app">
   <Header />
   <main>
